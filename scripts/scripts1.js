1d614195454d3993a9f51f55c8b6ef0d7b444d90
Tristan <tricooper@gmail.com>
2017-06-05 21:10:34 -0400

    removing scripts1.js

--- scripts/scripts1.js
+++
@@ -1,8 +0,0 @@
-bot.hear(/Hello!/, function(res) {
-  return res.send("Hi there!");
-});
-
-bot.respond(/What's your favorite food?/, function(res) {
-  return res.send("I'm a robot--I don't eat food!");
-});
-
