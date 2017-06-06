1054d829c0807b2fd8ac08a34490c6c3d4e5c272
Tristan <tricooper@gmail.com>
2017-06-05 20:05:56 -0400

    Initial Commit

--- scripts/scripts1.js
+++ scripts/scripts1.js
@@ -1,3 +1,8 @@
 bot.hear(/Hello!/, function(res) {
   return res.send("Hi there!");
 });
+
+bot.respond(/What's your favorite food?/, function(res) {
+  return res.send("I'm a robot--I don't eat food!");
+});
+
