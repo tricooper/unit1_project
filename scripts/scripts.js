8c65353f82309d3d8875d669009fcf4328296b56
Tristan <tricooper@gmail.com>
2017-06-08 20:57:41 -0400

    into one function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,13 +2,12 @@ module.exports = function(robot) {
     robot.hear(/Hello!/, function(res) {
       return res.send("Hi there!");
     });
+ 	robot.respond(/what is your favorite food?/, function(res) {
+		return res.send("I'm a robot -- I don't eat food!");
+	});   
 
 }
 
-// module.exports = function(robot) {
-// 	robot.respond(/what is your favorite food?/, function(res) {
-// 		return res.send("I'm a robot -- I don't eat food!");
-// 	});
-// }
+
 
 
