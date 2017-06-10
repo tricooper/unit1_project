b8f5739b4fd9f909e5f12a5da41bf9156d5df986
Tristan <tricooper@gmail.com>
2017-06-10 14:45:01 -0400

    changing to robot.respond

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,9 @@
 module.exports = function(robot) {
-    robot.hear(/Hello!/i, function(res) {
+    robot.respond(/Hello!/i, function(res) {
       return res.send("Hi there!");
     });
 
- 	robot.hear(/favorite food/i, function(res) {
+ 	robot.respond(/favorite food/i, function(res) {
 		return res.send("I'm a robot -- I don't eat food!");
 	});   
 
@@ -17,36 +17,36 @@ module.exports = function(robot) {
  		}
  	});
 
- 	robot.hear(/year/i, function(res) {
+ 	robot.respond(/year/i, function(res) {
  		var d = new Date();
 		var year = d.getFullYear();
  		return res.reply('the year is: ' + year);
  	});
 
- 	 	robot.hear(/day/i, function(res) {
+ 	 	robot.respond(/day/i, function(res) {
  		var d = new Date();
 		var day = d.getDay();
  		switch (day) {
  			case 0:
- 				return res.reply('the day is: Sunday "sunglasses:');
+ 				return res.reply('Sunday :sunglasses:');
  				break;
  			case 1:
- 				return res.reply('the day is: Monday. :sleeping:');
+ 				return res.reply('Monday :sleeping:');
  				break;
  			case 2:
- 				return res.reply('the day is: Tuesday');
+ 				return res.reply('Tuesday :moneybag:');
  				break;
  			case 3:
- 				return res.reply('the day is: Wednesday. :camel:');
+ 				return res.reply('Wednesday. :camel:');
  				break;
  			case 4:
- 				return res.reply('the day is: Thursday');
+ 				return res.reply('Thursday :soon:');
  				break;
  			case 5:
- 				return res.reply('the day is: Friday :beers:');
+ 				return res.reply('Friday :beers:');
  				break;
  			case 6:
- 				return res.reply('the day is: Saturday :golfer:');
+ 				return res.reply('Saturday :golfer:');
  				break; 			
 
  		}
