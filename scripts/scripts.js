040cef5f04b4aa96c14f66871e54e35358de4ed0
Tristan <tricooper@gmail.com>
2017-06-12 16:00:34 -0400

    regular expression function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -90,19 +90,107 @@ module.exports = function(robot) {
 
  	 });
 
- 	 // tell me our class syllabus
+ 	 // who won the NBA championship this year?
+ 	robot.respond(/NBA champion (.*)/i, function(res){
+ 		var nbaChampions = {
+		2016: 'Cleveland Cavaliers',
+		2015: 'Golden State Warriors',
+		2014: 'San Antonio Spurs',
+		2013: 'Miami Heat',
+		2012: 'Miami Heat',
+		2011: 'Dallas Mavericks',
+		2010: 'Los Angeles Lakers',
+		2009: 'Los Angeles Lakers',
+		2008: 'Boston Celtics',
+		2007: 'San Antonio Spurs',
+		2006: 'Miami Heat',
+		2005: 'San Antonio Spurs',
+		2004: 'Detroit Pistons',
+		2003: 'San Antonio Spurs',
+		2002: 'Los Angeles Lakers',
+		2001: 'Los Angeles Lakers',
+		2000: 'Los Angeles Lakers',
+		1999: 'San Antonio Spurs',
+		1998: 'Chicago Bulls',
+		1997: 'Chicago Bulls',
+		1996: 'Chicago Bulls',
+		1995: 'Houston Rockets',
+		1994: 'Houston Rockets',
+		1993: 'Chicago Bulls',
+		1992: 'Chicago Bulls',
+		1991: 'Chicago Bulls',
+		1990: 'Detroit Pistons',
+		1989: 'Detroit Pistons',
+		1988: 'Los Angeles Lakers',
+		1987: 'Los Angeles Lakers',
+		1986: 'Boston Celtics',
+		1985: 'Los Angeles Lakers',
+		1984: 'Boston Celtics',
+		1983: 'Philadelphia 76ers',
+		1982: 'Los Angeles Lakers',
+		1981: 'Boston Celtics',
+		1980: 'Los Angeles Lakers',
+		1979: 'Seattle SuperSonics',
+		1978: 'Washington Bullets',
+		1977: 'Portland Trail Blazers',
+		1976: 'Boston Celtics',
+		1976: 'New York Nets',
+		1975: 'Golden State Warriors',
+		1975: 'Kentucky Colonels',
+		1974: 'Boston Celtics',
+		1974: 'New York Nets',
+		1973: 'New York Knicks',
+		1973: 'Indiana Pacers',
+		1972: 'Los Angeles Lakers',
+		1972: 'Indiana Pacers',
+		1971: 'Milwaukee Bucks',
+		1971: 'Utah Stars',
+		1970: 'New York Knicks',
+		1970: 'Indiana Pacers',
+		1969: 'Boston Celtics',
+		1969: 'Oakland Oaks',
+		1968: 'Boston Celtics',
+		1968: 'Pittsburgh Pipers',
+		1967: 'Philadelphia 76ers',
+		1966: 'Boston Celtics',
+		1965: 'Boston Celtics',
+		1964: 'Boston Celtics',
+		1963: 'Boston Celtics',
+		1962: 'Boston Celtics',
+		1961: 'Boston Celtics',
+		1960: 'Boston Celtics',
+		1959: 'Boston Celtics',
+		1958: 'St. Louis Hawks',
+		1957: 'Boston Celtics',
+		1956: 'Philadelphia Warriors',
+		1955: 'Syracuse Nationals',
+		1954: 'Minneapolis Lakers',
+		1953: 'Minneapolis Lakers',
+		1952: 'Minneapolis Lakers',
+		1951: 'Rochester Royals',
+		1950: 'Minneapolis Lakers',
+		1949: 'Minneapolis Lakers',
+		1948: 'Baltimore Bullets',
+		1947: 'Philadelphia Warriors'
+		};
+ var champion = res.match[1];
+ var championTest = new RegExp(champion, 'i');
+ var text = 'The ' + champion + ' won the championship in: ';
 
- 	 // robot.respond(/.*syllabus.*/i, function(res) {
- 	 // 		var syllabus = {
+for (var i in nbaChampions) {
+  
+    if (championTest.test(nbaChampions[i])){
+      text += i + ', '; 
+  }
+}
+return res.reply (text);
 
- 	 // 		}
+ 	});
 
- 	 // });
  	 // what are we doing in class today?
  	 // use settimeout to delay the response
  	 // what is our zoom link?
 
- 	 // define x. (defines a certain piece of javascript)
  	  	
  	 //  	robot.respond(/tell me/i, function(res){
  	 //  		var room;
